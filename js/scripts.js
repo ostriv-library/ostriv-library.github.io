fa5c03b90870a3a53c4428c5da6c9ab8e3254d39
Roman Burdyga <roman.burdyga@gmail.com>
2025-06-02 12:19:59 -0300

    amount of books for book filter

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,13 @@ fetch(url)
 .then(data => {
 	jsonData = data;
 	sortAndRender();
+
+	const availableCount = jsonData.filter(item => item.Available === 'Yes').length;
+	const notAvailableCount = jsonData.filter(item => item.Available === 'No').length;
+
+	document.getElementById('allBooks').innerHTML = jsonData.length;	
+	document.getElementById('availableBooks').innerHTML = availableCount;	
+	document.getElementById('unavailableBooks').innerHTML= notAvailableCount;	
 })
 .catch(error => {
 	document.getElementById('data-container').innerText = 'Помилка: ' + error.message;
@@ -40,6 +47,7 @@ function renderData(data) {
 	data.forEach(item => {
 		const div = document.createElement('div');
 		div.className = 'col';
+		
 		div.innerHTML = `
 			<div class="card h-100">
 				<img src="${item.Cover}" class="card-img-top h-100" alt="${item.Name}">
@@ -50,14 +58,15 @@ function renderData(data) {
 							Опис
 						</a>
 					</p>
-					${item.Available === 'Yes' ? `<span class="badge rounded-pill text-bg-success"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-check-circle-fill " viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>&nbsp;&nbsp;Доступна</span>
-					` : `<span class="badge rounded-pill text-bg-danger"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/></svg>&nbsp;&nbsp;Зайнята</span>`}
-						<p class="card-text float-end">${item.Lang === 'UA' ? `<span class="bookLang h5" title="Українською">🇺🇦</span>`:`<span class="bookLang h5" title="Англійською">🇬🇧</span>`}</p>
-					
+			${item.Available === 'Yes' ? `<span class="badge rounded-pill text-bg-success"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-check-circle-fill " viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>&nbsp;&nbsp;Доступна</span>
+			` : `<span class="badge rounded-pill text-bg-danger"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/></svg>&nbsp;&nbsp;Зайнята</span>`}
+			<p class="card-text float-end">${item.Lang === 'UA' ? `<span class="bookLang h5" title="Українською">🇺🇦</span>`:`<span class="bookLang h5" title="Англійською">🇬🇧</span>`}</p>
+
 				</div>
 			</div>
 		`;
 		container.appendChild(div);
+		
 	});
 }
 
@@ -83,17 +92,17 @@ function sortAndRender() {
 
 // Перемикання сортування
 function toggleSortOrder() {
-  currentOrder = currentOrder === 'asc' ? 'desc' : 'asc';
+	currentOrder = currentOrder === 'asc' ? 'desc' : 'asc';
 
-  const sortButton = document.getElementById('sort-button');
-  const arrow = currentOrder === 'asc' ? `
+	const sortButton = document.getElementById('sort-button');
+	const arrow = currentOrder === 'asc' ? `
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-alpha-down" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"/><path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z"/></svg> Назва
-  ` : `
+		` : `
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-alpha-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"/><path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zm-8.46-.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707z"/></svg> Назва
-`;
-  sortButton.innerHTML = arrow;
+	`;
+	sortButton.innerHTML = arrow;
 
-  sortAndRender();
+	sortAndRender();
 }
 
 // Установка фільтру
