695571c348bf7087ec5aa47854e5e9010ec8d90c
Roman Burdyga <roman.burdyga@gmail.com>
2026-05-15 18:24:11 -0300

    cards design update

--- js/scripts.js
+++ js/scripts.js
@@ -78,20 +78,29 @@ function renderData(data) {
 		div.className = 'col';
 
 		div.innerHTML = `
-			<div class="card h-100">
+			<div class="card h-100 shadow-sm"  data-bs-toggle="modal" data-bs-target="#myModal"  data-description="${escapeHTML(item.Description)}">
 				<img src="${item.Cover}" class="card-img-top h-100" alt="${item.Name}">
-				<div class="card-body">
+				<div class="card-img-overlay  d-flex flex-column justify-content-end">
+				<div>
 					<h6 class="card-title">${item.Name}</h6>
 					<p class="card-text text-truncate"><small class="text-body-secondary">${item.Author}</small><br>
-						<a href="#myModal" class="" data-description="${escapeHTML(item.Description)}" data-bs-toggle="modal">
+						<!--<a href="#myModal" class="" data-description="${escapeHTML(item.Description)}" data-bs-toggle="modal">
 							Опис
-						</a>
+						</a>-->
 					</p>
 			${item.Available === 'Yes' ? `<span class="badge rounded-pill text-bg-success"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-check-circle-fill " viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>&nbsp;&nbsp;Доступна</span>
 			` : `<span class="badge rounded-pill text-bg-danger"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/></svg>&nbsp;&nbsp;Зайнята</span>`}
 			<p class="card-text float-end">${item.Lang === 'UA' ? `<span class="bookLang h5" title="Українською">🇺🇦</span>`:`<span class="bookLang h5" title="Англійською">🇬🇧</span>`}</p>
-
 				</div>
+				</div>
+				 <div class="gradient-blur">
+				    <div></div>
+				    <div></div>
+				    <div></div>
+				    <div></div>
+				    <div></div>
+				    <div></div>
+				  </div>
 			</div>
 		`;
 		container.appendChild(div);
